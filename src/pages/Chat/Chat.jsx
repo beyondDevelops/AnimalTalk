ed0c1e7d988e55682e4ba9ba88ac343de3cd7b62
GreattitJY <great.tit.jaejae@gmail.com>
2022-12-13 09:43:42 +0900

    setting: Tailwind 환경 설정
    Co-authored-by: July249 <jhlee7125@gmail.com>
    Co-authored-by: GreattitJY <great.tit.jaejae@gmail.com>
    Co-authored-by: IntHyun <wjdtngus1237@naver.com>

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Chat = () => {
-  return <div>Chat</div>;
+  return <div className="text-red-400">Chat 나와라</div>;
 };
--- src/share/Footer.jsx
+++ src/share/Footer.jsx
@@ -5,7 +5,9 @@ export const Footer = () => {
   return (
     <>
       <Link to="/">home</Link>
-      <Link to="/chat">chat</Link>
+      <Link to="/chat" className="text-red-400">
+        chat
+      </Link>
       <Link to="postcreate">postcreate</Link>
       <Link to="myprofile">myprofile</Link>
     </>
