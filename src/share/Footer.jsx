9aa9f887c93956ba0bb2f0b49b624e71fb59356d
정수현 <68059880+IntHyun@users.noreply.github.com>
2022-12-13 17:43:48 +0900

    style: Navigate 삭제
    기능이랑 상관없는 것이 import되어 있어 삭제했습니다.

--- src/share/Footer.jsx
+++ src/share/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   const home = `${process.env.PUBLIC_URL}/assets/img/icon-home.png`;
