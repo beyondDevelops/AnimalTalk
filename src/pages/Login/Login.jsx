28d241f6fe75dce83f12694d0e7a6c2f74dc90f6
JeongSuHyun <68059880+baejeong12@users.noreply.github.com>
2022-12-14 16:05:40 +0900

    fix: border 변수읽지 못하는 버그 수정

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,35 @@
 import LoginBtn from "../../components/LoginBtn/LoginBtn";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const loginImg = `${process.env.PUBLIC_URL}/assets/img/char-login-cat.svg`;
   const kakaoImg = `${process.env.PUBLIC_URL}/assets/img/logo-kakao.png`;
   const googleImg = `${process.env.PUBLIC_URL}/assets/img/logo-google.png`;
   const faceBookImg = `${process.env.PUBLIC_URL}/assets/img/logo-facebook.png`;
+
+  const loginBtnStyles = [
+    {
+      id: "kakao",
+      imgUrl: kakaoImg,
+      color: "border-[#F2C94C]",
+      text: "카카오톡 계정으로 로그인",
+      mt: "mt-[50px]",
+    },
+    {
+      id: "google",
+      imgUrl: googleImg,
+      color: "border-[#767676]",
+      text: "카카오톡 계정으로 로그인",
+      mt: "mt-[10px]",
+    },
+    {
+      id: "facebook",
+      imgUrl: faceBookImg,
+      color: "border-[#2D9CDB]",
+      text: "카카오톡 계정으로 로그인",
+      mt: "mt-[10px]",
+    },
+  ];
 
   return (
     <main className="page">
@@ -13,19 +38,18 @@ const Login = () => {
       </div>
       <article className="bg-slate-200 h-3/5">
         <ul className="flex flex-col items-center">
-          <LoginBtn img={kakaoImg} text="카카오톡 계정으로 로그인" mt="mt-[50px]" border="#F2C94C" />
-          <LoginBtn img={googleImg} text="구글 계정으로 로그인" mt="mt-[10px]" border="#767676" />
-          <LoginBtn img={faceBookImg} text="페이스북 계정으로 로그인" mt="mt-[10px]" border="#2D9CDB" />
+          {loginBtnStyles.map((item) => (
+            <LoginBtn id={item.id} img={item.imgUrl} borderColor={item.color} text={item.text} mt={item.mt} />
+          ))}
         </ul>
-        <ul className="flex justify-center gap-[28px] mt-[20px] text-[#767676] text-[12px]">
-          {/* 추후에 버튼을 링크로 바꿀 예정입니다. */}
-          <button>
-            <li>이메일로 로그인</li>
-          </button>
-          <button>
-            <li>회원가입</li>
-          </button>
-        </ul>
+        <div className="flex justify-center gap-[28px] mt-[20px] text-[#767676] text-[12px]">
+          <Link to="/login/email">
+            <button>이메일로 로그인</button>
+          </Link>
+          <Link to="/join">
+            <button>회원가입</button>
+          </Link>
+        </div>
       </article>
     </main>
   );
