fd665898dd892dc27d5c3218052041487a42d27f
JeongSuHyun <68059880+baejeong12@users.noreply.github.com>
2022-12-14 09:57:39 +0900

    feat: Splash Screen 화면 구현

--- src/pages/SplashScreen/SplashScreen.jsx
+++ src/pages/SplashScreen/SplashScreen.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const SplashScreen = () => {
-  return <div>SplashScreen</div>;
+  const splashImg = `${process.env.PUBLIC_URL}/assets/img/char-loading-cat.svg`;
+  return (
+    <main className="page">
+      <div className="bg-cyan-300 basis-full overflow-hidden">
+        <img
+          src={splashImg}
+          alt=""
+          className="w-[145px] h-[200px] mt-64 mx-[auto]"
+        />
+      </div>
+    </main>
+  );
 };
 
 export default SplashScreen;
