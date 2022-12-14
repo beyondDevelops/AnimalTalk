240613f995b5b483478a679b65cea2a4ca88ea90
정수현 <68059880+IntHyun@users.noreply.github.com>
2022-12-14 16:43:35 +0900

    chore: li에 마진바텀 추가

--- src/components/LoginBtn/LoginBtn.jsx
+++ src/components/LoginBtn/LoginBtn.jsx
@@ -2,7 +2,7 @@ const LoginBtn = ({ mt, img, text, borderColor }) => {
   return (
     <li className={mt}>
       <button
-        className={`rounded-[44px] min-w-[320px] min-h-[44px] leading-[44px] ${borderColor} border-[1px] bg-white text-center relative text-[#767676]`}
+        className={`mb-[10px] rounded-[44px] min-w-[320px] min-h-[44px] leading-[44px] ${borderColor} border-[1px] bg-white text-center relative text-[#767676]`}
       >
         <img src={img} alt="" className="w-[18px] h-[18px] inline absolute left-[20px] top-[50%] translate-y-[-50%]" />
         {text}
