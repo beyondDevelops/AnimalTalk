f1e060800cb9cf15a72b2436627930bc583d9478
jihyunglee <jhlee7125@gmail.com>
2022-12-16 10:43:31 +0900

    refactor: HeaderBind.jsx  header 스타일 수정

--- src/share/HeaderBind.jsx
+++ src/share/HeaderBind.jsx
@@ -7,19 +7,20 @@ const HeaderBasic = () => {
   let navigate = useNavigate();
 
   return (
-    <header className="w-[390px] bg-emerald-300 h-[48px] flex shrink-0 justify-between items-center">
+    <header className="flex justify-between items-center pl-[1.6rem] pr-[1.2rem]">
       <button
+        className="w-[2.2rem] h-[2.2rem]"
         type="type"
         onClick={() => {
           navigate(-1);
         }}
       >
-        <img src={arrowLeft} alt="뒤로가기" className="w-[22px] h-[22px] ml-[18px]" />
+        <img src={arrowLeft} alt="뒤로가기" />
       </button>
 
       {/* 아래 버튼에는 모달 이벤트가 들어가야 합니다. */}
-      <button type="button">
-        <img src={moreVertical} alt="더보기" className="w-[24px] h-[24px] mr-[12px]" />
+      <button className="w-[2.4rem] h-[2.4rem] my-[1.2rem]" type="button">
+        <img src={moreVertical} alt="더보기" />
       </button>
     </header>
   );
@@ -30,24 +31,25 @@ const HeaderSearch = () => {
   let navigate = useNavigate();
 
   return (
-    <header className="w-[390px] bg-emerald-300 h-[48px] flex justify-between shrink-0 items-center">
+    <header className="flex justify-between items-center px-[1.6rem]">
       <button
+        className="w-[2.2rem] h-[2.2rem]"
         type="button"
         onClick={() => {
           navigate(-1);
         }}
       >
-        <img src={arrowLeft} alt="뒤로가기" className="w-[22px] h-[22px] ml-[18px]" />
+        <img src={arrowLeft} alt="뒤로가기" />
       </button>
 
-      <form action="">
+      <form action="" className="py-[0.8rem]">
         <fieldset>
           <legend className="ir">검색창</legend>
           <label htmlFor="search"></label>
           <input
             id="search"
             type="text"
-            className="w-[316px] h-[32px] bg-[#F2F2F2] rounded-[32px] mr-[19px] px-[16px] py-[7px] focus:borde"
+            className="w-[316px] bg-[#F2F2F2] rounded-[32px] px-[16px] py-[7px] text-[1.4rem] focus:border"
             placeholder="계정 검색"
           />
         </fieldset>
@@ -59,11 +61,11 @@ const HeaderSearch = () => {
 const HeaderFeed = () => {
   const search = `${process.env.PUBLIC_URL}/assets/img/icon-search.png`;
   return (
-    <header className="w-[390px] bg-sky-300 h-[48px] flex shrink-0 justify-between items-center">
-      <span className="ml-[16px] text-[18px] font-medium">애니멀톡 피드</span>
+    <header className="flex justify-between items-center px-[1.6rem]">
+      <span className="text-[18px] font-medium">애니멀톡 피드</span>
       {/* button을 Link로 바꾸어 serach페이지로 이동해야합니다. */}
-      <button type="button">
-        <img src={search} alt="검색하기" className="w-[16px] h-[16px] mr-[17px]" />
+      <button className="w-[2.4rem] h-[2.4rem] my-[1.2rem]" type="button">
+        <img src={search} alt="검색하기" />
       </button>
     </header>
   );
@@ -74,16 +76,17 @@ const HeaderSave = () => {
   let navigate = useNavigate();
 
   return (
-    <header className="w-[390px] bg-sky-300 h-[48px] flex justify-between shrink-0 items-center">
+    <header className="flex justify-between items-center px-[1.6rem]">
       <button
+        className="w-[2.2rem] h-[2.2rem]"
         type="button"
         onClick={() => {
           navigate(-1);
         }}
       >
-        <img className="w-[22px] h-[22px] ml-[18px]" src={arrowLeft} alt="뒤로가기" />
+        <img className="" src={arrowLeft} alt="뒤로가기" />
       </button>
-      <button className="btn-md bg-[#FCD690] text-[#fff] mr-[16px]">저장</button>
+      <button className="btn-md bg-[#FCD690] text-[1.4rem] text-[#fff] my-[0.8rem]">저장</button>
     </header>
   );
 };
@@ -94,21 +97,22 @@ const HeaderChat = () => {
   let navigate = useNavigate();
 
   return (
-    <header className="w-[390px] bg-emerald-300 h-[48px] flex justify-between shrink-0 items-center">
+    <header className="flex justify-between items-center px-[1.6rem]">
       <button
+        className="w-[2.2rem] h-[2.2rem]"
         type="button"
         onClick={() => {
           navigate(-1);
         }}
       >
-        <img src={arrowLeft} alt="뒤로가기" className="w-[22px] h-[22px] ml-[18px]" />
+        <img src={arrowLeft} alt="뒤로가기" />
       </button>
       {/* 아래 텍스트는 유저 이름을 porps로 전달받습니다. */}
-      <span className="ml-[15px] basis-full">안녕하세요. 환영합니다.</span>
+      <span className="ml-[10px] mr-auto text-[1.4rem]">안녕하세요. 환영합니다.</span>
 
       {/* 아래 버튼에는 모달 이벤트가 들어가야 합니다. */}
-      <button type="button">
-        <img src={moreVertical} alt="더보기" className="w-[24px] h-[24px] mr-[12px]" />
+      <button className="w-[2.4rem] h-[2.4rem] my-[1.2rem]" type="button">
+        <img src={moreVertical} alt="더보기" />
       </button>
     </header>
   );
