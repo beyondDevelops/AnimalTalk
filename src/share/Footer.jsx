7e78b7c97e144d19005ee952416accf824bad80e
jihyunglee <90930391+July249@users.noreply.github.com>
2022-12-16 16:28:17 +0900

    refactor: Footer.jsx 스타일 수정 누락 사항 추가

--- src/share/Footer.jsx
+++ src/share/Footer.jsx
@@ -11,19 +11,19 @@ const Footer = () => {
     <nav className="flex px-[0.6rem] pt-[1.2rem] pb-[0.6rem]">
       <Link className="flex flex-col items-center mx-[3rem]" to="/">
         <img src={home} alt="홈" className="w-[2.4rem] h-[2.4rem]" />
-        <span className="block mt-[0.4rem] text-[10px] text-[#767676]">홈</span>
+        <span className="block mt-[0.4rem] text-[1rem] text-[#767676]">홈</span>
       </Link>
       <Link className="flex flex-col items-center mx-[3rem]" to="/chat">
         <img src={chat} alt="채팅" className="w-[2.4rem] h-[2.4rem]" />
-        <span className="block mt-[0.4rem] text-[10px] text-[#767676]">채팅</span>
+        <span className="block mt-[0.4rem] text-[1rem] text-[#767676]">채팅</span>
       </Link>
       <Link className="flex flex-col items-center mx-[3rem]" to="/postcreate">
         <img src={postCreate} alt="게시글 작성" className="w-[2.4rem] h-[2.4rem]" />
-        <span className="block mt-[0.4rem] text-[10px] text-[#767676]">게시물 작성</span>
+        <span className="block mt-[0.4rem] text-[1rem] text-[#767676]">게시물 작성</span>
       </Link>
       <Link className="flex flex-col items-center mx-[3rem]" to="/myprofile">
         <img src={myProfile} alt="프로필" className="w-[2.4rem] h-[2.4rem]" />
-        <span className="block mt-[0.4rem] text-[10px] text-[#767676]">프로필</span>
+        <span className="block mt-[0.4rem] text-[1rem] text-[#767676]">프로필</span>
       </Link>
     </nav>
   );
