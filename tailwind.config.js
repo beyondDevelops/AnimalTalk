f8fbf827ebbd6deee8787374abd9a98b1c435dd1
jihyunglee <jhlee7125@gmail.com>
2022-12-18 12:13:54 +0900

    settiing: 중요 color 정보 변수화

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
+      colors: {
+        "main-color": "#EDA751",
+        "sub-color": "#FCD690",
+        "custom-gray": "#767676",
+        "custom-light-gray": "#dbdbdb",
+        "custom-layout-color": "#f3f4f6",
+      },
       fontFamily: {
         spoqa: ["Spoqa Han Sans Neo", "sans-serif"],
       },
