4a6cc2c566662e797734ef23d50c44560082e44d
GreattitJY <great.tit.jaejae@gmail.com>
2022-12-20 23:48:16 +0900

    chore: post함수 변경으로 인한 호출 변경

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,15 +3,10 @@ import Footer from "../../share/Footer";
 import { HeaderFeed } from "../../share/HeaderBind";
 
 const Home = () => {
-  const data = [{ id: 1 }, { id: 2 }, { id: 3 }];
   return (
     <div className="page">
       <HeaderFeed />
-      <main>
-        {data.map((item) => (
-          <Post key={item.id} />
-        ))}
-      </main>
+      <main></main>
       <Footer />
     </div>
   );
