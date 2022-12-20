0e58100846b2ee9256c6f1711934eb410eabf28a
정수현 <68059880+IntHyun@users.noreply.github.com>
2022-12-20 13:16:59 +0900

    chore: section태그 추가, px -> rem변경

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Modal = ({ content, value, handleModalLogout }) => {
+const Modal = ({ content, value, onModalClose }) => {
   return (
-    <div className="bg-[#d4b886] w-[252px] h-[110px] absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] rounded-[10px] z-10">
-      <p className="text-center py-[22px] font-medium">{content}</p>
-      <ul className="flex border-t-[1px]">
-        <li className="basis-[126px] h-[43px] text-center leading-[43px] border-r-[1px]">
-          <button onClick={handleModalLogout} className="w-[100%] hover:text-[#ff0000]">
-            취소
-          </button>
-        </li>
-        <li className="basis-[126px] h-[43px] text-center leading-[43px] hover:text-[#ff0000]">
-          {content === "로그아웃하시겠어요?" ? (
-            // 이때 로그아웃 버튼을 클릭하면 Home으로 이동함과 동시에 userInfo를 false로 바꿔주면 될 것 같습니다.
-            <Link to="/" className="w-[100%] inline-block">
-              {value}
-            </Link>
-          ) : (
-            <button className="w-[100%]">{value}</button>
-          )}
-        </li>
-      </ul>
-    </div>
+    <section className="absolute inset-0 z-10">
+      <h2 className="ir">로그아웃 창</h2>
+      <div className="bg-[#d4b886] w-[25.2rem] h-[11rem] absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] rounded-[1rem]">
+        <h3 className="text-center py-[2.2rem] font-medium">{content}</h3>
+        <ul className="flex border-t-[0.1rem]">
+          <li className="basis-[12.6rem] text-center leading-[4.3rem] border-r-[0.1rem]">
+            {/* 아래의 함수를 수정 */}
+            <button onClick={onModalClose} className="w-[100%] hover:text-[#ff0000]">
+              취소
+            </button>
+          </li>
+          <li className="basis-[12.6rem] text-center leading-[4.3rem] hover:text-[#ff0000]">
+            {value === "로그아웃" ? (
+              // 이때 로그아웃 버튼을 클릭하면 Home으로 이동함과 동시에 userInfo를 false로 바꿔주면 될 것 같습니다.
+              <Link to="/login" className="w-[100%] inline-block">
+                {value}
+              </Link>
+            ) : (
+              <button className="w-[100%]">{value}</button>
+            )}
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 };
 
