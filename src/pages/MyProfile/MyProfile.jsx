ba76894a9170d4631a94d32398480326c7895077
정수현 <68059880+IntHyun@users.noreply.github.com>
2022-12-20 13:42:34 +0900

    refactor: handleCloseModal함수 추가, 전달되는 함수 on으로 변경

--- src/pages/MyProfile/MyProfile.jsx
+++ src/pages/MyProfile/MyProfile.jsx
@@ -6,41 +6,54 @@ import Post from "../../share/Post";
 import Club from "../../share/Club";
 import ModalInfo from "../../components/ModalInfo/ModalInfo";
 import Modal from "../../components/Modal/Modal";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useRef } from "react";
 
 const MyProfile = () => {
-  let [modal, setModal] = useState(false);
-  let [logout, setLogout] = useState(false);
-  let [blur, setBlur] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [logout, setLogout] = useState(false);
+  const [blur, setBlur] = useState(false);
+  const modalRef = useRef();
 
-  const handleModalInfo = useCallback(() => {
-    setModal(!modal);
-  });
+  const handleModalInfo = useCallback(
+    (e) => {
+      setModal(!modal);
+      if (e.target === modalRef.current) {
+        setModal(!modal);
+      }
+    },
+    [modal]
+  );
 
   const handleModalLogout = useCallback(() => {
     setLogout(!logout);
     setBlur(!blur);
-  });
+  }, [logout, blur]);
+
+  const handleCloseModal = useCallback(() => {
+    setLogout(false);
+    setBlur(false);
+  }, []);
 
   return (
     <div className="bg-gray-100">
-      <div className="page">
-        {logout === true ? (
-          <Modal content="로그아웃하시겠어요?" value="로그아웃" handleModalLogout={handleModalLogout} />
-        ) : null}
-        <HeaderBasic handleModalInfo={handleModalInfo} />
-        <main className={`h-screen overflow-y-auto scrollbar-hide ${blur ? "blur-sm" : null}`}>
+      {logout === true ? (
+        <Modal modalRef={modalRef} content="로그아웃하시겠어요?" value="로그아웃" onModalClose={handleCloseModal} />
+      ) : null}
+      <div className={`page ${blur ? "blur-sm" : null}`}>
+        <HeaderBasic onModalInfo={handleModalInfo} />
+        <main className={`h-screen overflow-y-auto scrollbar-hide`}>
           <UserProfile />
           <Club />
-          <Post handleModalInfo={handleModalInfo} handleModalLogout={handleModalLogout} />
+          <Post onModalInfo={handleModalInfo} onModalAction={handleModalLogout} />
         </main>
         <Footer />
         {modal === true ? (
           <ModalInfo
+            modalRef={modalRef}
             contentOne="설정 및 개인정보"
             contentTwo="로그아웃"
-            handleModalInfo={handleModalInfo}
-            handleModalLogout={handleModalLogout}
+            onModalInfo={handleModalInfo}
+            onModalAction={handleModalLogout}
           />
         ) : null}
       </div>
