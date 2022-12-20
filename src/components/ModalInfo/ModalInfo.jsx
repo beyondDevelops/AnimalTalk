4b0c6c4d0bcbf0eac52ccd2a21caf0a6624c1d94
정수현 <68059880+IntHyun@users.noreply.github.com>
2022-12-20 13:17:44 +0900

    chore: section태그 추가, px -> rem변경

--- src/components/ModalInfo/ModalInfo.jsx
+++ src/components/ModalInfo/ModalInfo.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 
-const ModalInfo = ({ handleModalLogout, handleModalInfo, contentOne, contentTwo }) => {
+const ModalInfo = ({ onModalAction, onModalInfo, contentOne, contentTwo, modalRef }) => {
   return (
-    <div className="absolute bottom-0 bg-[#d4b886] w-[390px] h-[138px] rounded-t-[10px]">
-      <button
-        onClick={handleModalInfo}
-        className="bg-[#DBDBDB] border-[2px] rounded-[5px] block w-[50px] mx-auto my-[16px]"
-      ></button>
-      <button className="block p-[12.5px] w-[100%] text-left pl-[26px]">{contentOne}</button>
-      <button
-        onClick={handleModalLogout}
-        className="border-t-[1px] p-[12.5px]  w-[100%] text-left pl-[26px] hover:text-[#ff0000]"
-      >
-        {contentTwo}
-      </button>
-    </div>
+    <section onClick={onModalInfo} ref={modalRef} className="absolute inset-0">
+      <h2 className="ir">모달 설정창</h2>
+      <div className="absolute bottom-0 left-[50%] translate-x-[-50%] bg-[#d4b886] w-[39rem] rounded-t-[1rem] mx-auto">
+        <button className="bg-[#DBDBDB] border-[0.2rem] rounded-[0.5rem] block w-[5rem] mx-auto my-[1.6rem]"></button>
+        <button
+          onClick={() => {
+            alert("준비중입니다.");
+          }}
+          className="block p-[1.25rem] w-[100%] text-left pl-[2.6rem]"
+        >
+          {contentOne}
+        </button>
+        <button
+          onClick={onModalAction}
+          className="border-t-[0.1rem] p-[1.25rem]  w-[100%] text-left pl-[2.6rem] hover:text-[#ff0000]"
+        >
+          {contentTwo}
+        </button>
+      </div>
+    </section>
   );
 };
 
