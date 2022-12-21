73d1780587a9b1085cf3c0aa0f012af7df8acfe5
GreattitJY <great.tit.jaejae@gmail.com>
2022-12-21 22:11:05 +0900

    design: 채팅 리스트의 날짜 스타일 추가

--- src/shared/SimpleUserList/SimpleUserList.jsx
+++ src/shared/SimpleUserList/SimpleUserList.jsx
@@ -6,9 +6,10 @@ const SimpleUserList = ({ isBtn }) => {
   // const isBtn = true;
   const userName = "사용자 이름";
   // Note::아래 셋 중 하나가 사용됩니다. nullish를 사용해서 0이 아닌 null 값이 들어가야 됩니다.
-  const accountName = "계정 아이디";
-  const content = "프로필 소개글";
-  const chat = "마지막 대화 내역";
+  // Note:: props로 데이터를 전달받아서 사용하면 편할 거 같습니다.
+  const accountName = null;
+  const content = null;
+  const chat = "마지막 대화 내역입니다. 가나다라마바사아자카타파하";
   // Note::useState(false)안의 인자값을 props로 받고 이벤트 발생 시 axios로 데이터를 전송하는 건 어떨까요?
   const [isFollow, setIsFollow] = useState(false);
 
@@ -21,10 +22,12 @@ const SimpleUserList = ({ isBtn }) => {
   return (
     <li className="mx-[1.6rem] my-[1.6rem] flex justify-between items-center">
       <img src={tmpImg} alt="" className="w-[5rem] h-[5rem]" />
-      <div className="mr-auto ml-[1.2rem]">
+      <p className="mr-auto ml-[1.2rem]">
         <strong className="font-medium">{userName}</strong>
-        <p className="text-[1.2rem] text-cst-gray">{accountName ?? content ?? chat}</p>
-      </div>
+        <span className="block text-[1.2rem] w-[23.8rem] whitespace-nowrap text-cst-gray text-ellipsis overflow-hidden">
+          {accountName ?? content ?? chat}
+        </span>
+      </p>
 
       {isBtn ? (
         <button
@@ -36,7 +39,9 @@ const SimpleUserList = ({ isBtn }) => {
         </button>
       ) : (
         <>
-          <time>2022.12.21</time>
+          <time dateTime="2022-12-21" className="mt-[2.3rem] text-[1rem] text-cst-light-gray">
+            2022.12.21
+          </time>
         </>
       )}
     </li>
