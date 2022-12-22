5fba143cdbd0aa3018b9d3a84e2f0b0fa777a0e6
GreattitJY <great.tit.jaejae@gmail.com>
2022-12-22 16:39:39 +0900

    refactor: props로 데이터를 전달받아 조건부 렌더링 추가

--- src/shared/Header/HeaderSave.jsx
+++ src/shared/Header/HeaderSave.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 
-export const HeaderSave = () => {
+export const HeaderSave = ({ btnText, isActive }) => {
   const arrowLeft = `${process.env.PUBLIC_URL}/assets/img/icon-arrow-left.png`;
   let navigate = useNavigate();
 
@@ -15,7 +15,9 @@ export const HeaderSave = () => {
       >
         <img className="" src={arrowLeft} alt="뒤로가기" />
       </button>
-      <button className="btn-md bg-s-color text-[1.4rem] text-[#fff] my-[0.8rem]">저장</button>
+      <button className={`btn-md text-[1.4rem] text-[#fff] my-[0.8rem] ${isActive ? "bg-m-color" : "bg-s-color"}`}>
+        {btnText}
+      </button>
     </header>
   );
 };
