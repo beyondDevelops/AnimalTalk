b657c57f61582bc1a8a8b3a56b69783f48ddaad3
JeongSuHyun <68059880+baejeong12@users.noreply.github.com>
2022-12-24 19:53:45 +0900

    feat: 이메일검증 구현, 입력값에 따라 버튼 비활성화 기능 구현

--- src/pages/LoginEmail/LoginEmail.jsx
+++ src/pages/LoginEmail/LoginEmail.jsx
@@ -1,12 +1,17 @@
-import React, { useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import api from "../../api/axios";
 
-const LoginEmail = ({ name, btnName, option }) => {
+const LoginEmail = () => {
   const navigate = useNavigate();
-
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
+
+  const [isWrong, setIswrong] = useState(true);
+  const [emailLength, setEmailLength] = useState(0);
+  const [passwordLength, setPasswordLength] = useState(0);
+  const [btnColor, setBtnColor] = useState(false);
+  const [btnDisabled, setBtnDisabled] = useState(true);
 
   /* 이메일, 비밀번호 타이핑할 때 바로 추적하면서 유효성 검사 */
 
@@ -18,6 +23,24 @@ const LoginEmail = ({ name, btnName, option }) => {
   useEffect(() => {
     emailRef.current.focus();
   }, []);
+
+  useEffect(() => {
+    if (emailLength >= 1 && passwordLength >= 1) {
+      setBtnColor(true);
+      setBtnDisabled(false);
+    } else {
+      setBtnColor(false);
+      setBtnDisabled(true);
+    }
+  }, [emailLength, passwordLength]);
+
+  const handleEmailLength = () => {
+    setEmailLength(emailRef.current.value.length);
+  };
+
+  const handlePasswordLength = () => {
+    setPasswordLength(passwordRef.current.value.length);
+  };
 
   const handleUserLoginSubmit = async (e) => {
     /* 로그인 버튼을 누르면 https://mandarin.api.weniv.co.kr/user/login 에 post 메서드로 데이터 서버로 송신 */
@@ -37,6 +60,7 @@ const LoginEmail = ({ name, btnName, option }) => {
       );
       if (res.status !== 200) throw new Error("서버로부터의 통신에 실패하였습니다.");
       if (res.data.status) {
+        setIswrong(!isWrong);
         console.error(`${res.data.status}: ${res.data.message}`);
         return;
       }
@@ -63,15 +87,15 @@ const LoginEmail = ({ name, btnName, option }) => {
             </label>
             <input
               required
+              ref={emailRef}
+              onChange={handleEmailLength}
               id="emailId"
               type="email"
               className="w-[32.2rem] border-b-[1px] py-[0.8rem] border-cst-light-gray outline-none"
-              ref={emailRef}
-              // onChange={}
             />
           </fieldset>
 
-          <fieldset>
+          <fieldset className="mt-[1.5rem]">
             <legend className="ir">로그인</legend>
             <label htmlFor="pw" className="block text-[1.2rem] text-cst-gray py-[0.4rem]">
               비밀번호
@@ -79,15 +103,28 @@ const LoginEmail = ({ name, btnName, option }) => {
             <input
               required
               id="pw"
+              ref={passwordRef}
+              onChange={handlePasswordLength}
               type="password"
               className="w-[32.2rem] border-b-[1px] py-[0.8rem] border-cst-light-gray outline-none"
-              ref={passwordRef}
-              // onChange={}
             />
+            {isWrong ? null : (
+              <p className="absolute font-normal text-[1.2rem] text-[#EB5757] mt-[0.6rem]">
+                * 이메일 또는 비밀번호가 일치하지 않습니다.
+              </p>
+            )}
           </fieldset>
 
-          <button className="btn-xl bg-s-color text-[#fff] mt-[3rem] mb-[2rem] text-center">로그인</button>
+          <button
+            disabled={btnDisabled}
+            className={`btn-xl ${btnColor ? "btn-on" : "btn-off"} text-[#fff] mt-[6rem] mb-[2rem] text-center`}
+          >
+            로그인
+          </button>
         </form>
+        <Link to="/signup" className="text-cst-gray text-[1.2rem] text-center">
+          이메일로 회원가입
+        </Link>
       </main>
     </div>
   );
