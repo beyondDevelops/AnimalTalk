edb0e67e79e12db4e6dfaf33504b4de3d1c86d71
jihyunglee <jhlee7125@gmail.com>
2022-12-26 17:24:14 +0900

    feat: IntersectionObserver 객체 사용

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,34 +3,56 @@ import Post from "../../shared/Post/Post";
 import NoFeed from "../../components/NoFeed/NoFeed";
 import Footer from "../../shared/Footer/Footer";
 import axios from "../../api/axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
+  const [state, setState] = useState({ postNum: 0, moreFeed: true });
+
+  const observerTarget = useRef(null);
 
   const token = localStorage.getItem("token");
 
   useEffect(() => {
+    if (!observerTarget.current || !state.moreFeed) return;
+
     const getFollowersFeeds = async () => {
       try {
-        const res = await axios.get("/post/feed", {
+        const res = await axios.get(`/post/feed?limit=10&skip=${state.postNum}`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
-        setPosts(res.data.posts);
+        setPosts((prev) => [...prev, ...res.data.posts]);
+        setState((prev) => ({ postNum: prev.postNum + 10, moreFeed: posts.length % 10 === 0 }));
       } catch (err) {
         console.log(err);
       }
     };
 
-    getFollowersFeeds();
-  }, []);
+    const observerCallback = (entries, observer) => {
+      if (entries[0].isIntersecting) {
+        getFollowersFeeds();
+      }
+    };
+
+    const observer = new IntersectionObserver(observerCallback);
+    observer.observe(observerTarget.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [posts.length, state.postNum, state.moreFeed, token]);
 
   return (
     <div className="page">
       <HeaderFeed />
-      <main>{posts.length > 0 ? posts.map((post) => <Post key={post.id} post={post} />) : <NoFeed />}</main>
+      <main>
+        <>
+          {posts.length > 0 ? posts.map((post) => <Post key={post.id} post={post} />) : <NoFeed />}
+          <div ref={observerTarget}></div>
+        </>
+      </main>
       <Footer />
     </div>
   );
