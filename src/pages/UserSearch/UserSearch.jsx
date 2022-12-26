806c7d869a0010f9fcfb998cd33bb7270b455bfb
jihyunglee <jhlee7125@gmail.com>
2022-12-26 10:39:56 +0900

    feat: 계정 검색 기능 구현 및 검색 결과 filter 기능 구현, SimpleUserList 공유 컴포넌트에 props로 검색 결과 데이터 전달

--- src/pages/UserSearch/UserSearch.jsx
+++ src/pages/UserSearch/UserSearch.jsx
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { HeaderSearch } from "../../shared/Header/HeaderSearch";
 import SimpleUserList from "../../shared/SimpleUserList/SimpleUserList";
+import api from "../../api/axios";
 
 const Search = () => {
+  const token = localStorage.getItem("token");
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    if (search) {
+      const findUser = async () => {
+        try {
+          const res = await api.get(`/user/searchuser/?keyword=${search}`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          setUserList(res.data);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
+      findUser();
+    } else {
+      setUserList([]);
+    }
+  }, [search, token]);
+
+  useEffect(() => {
+    if (search) {
+      const filterUsers = userList.filter(
+        (user) =>
+          user.username.toLowerCase().includes(search.toLowerCase()) ||
+          user.accountname.toLowerCase().includes(search.toLowerCase())
+      );
+      setSearchResult(filterUsers.reverse());
+    }
+  }, [userList, search]);
+
   return (
     <div className="page">
-      <HeaderSearch />
-      <SimpleUserList isBtn={false} />
+      <HeaderSearch search={search} setSearch={setSearch} />
+      {userList.map((user) => (
+        <SimpleUserList
+          key={user._id}
+          isMessage={false}
+          isBtn={false}
+          isChatMode={false}
+          username={user.username}
+          accountname={user.accountname}
+          profileImg={user.image}
+        />
+      ))}
     </div>
   );
 };
