ee4b259baee56737f8855dfbf1c5b4f8196123e1
jihyunglee <jhlee7125@gmail.com>
2022-12-26 22:15:26 +0900

    feat: UserFeed에서 요청한 Club api 데이터를 이용하여 SimpleClub에서 데이터 처리

--- src/components/SimpleClub/SimpleClub.jsx
+++ src/components/SimpleClub/SimpleClub.jsx
@@ -1,17 +1,17 @@
-import { Link, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const SimpleClub = (props) => {
-  const { club } = useParams();
-
-  const clubThumbnailImg = props.data.thumbnailImg
-    ? props.data.thumbnailImg
+const SimpleClub = ({ data }) => {
+  const navigate = useNavigate();
+  const clubId = data.id;
+  const clubThumbnailImg = data.itemImage
+    ? data.itemImage
     : "https://images.unsplash.com/photo-1540411003967-af56b79be677?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80";
-  const clubTitle = props.data.title;
-  const clubLocation = props.data.location;
+  const clubTitle = data.itemName;
+  const clubFee = data.price;
 
   return (
     <li className="shrink-0 basis-[14rem] mr-[1rem] overflow-hidden">
-      <Link to={`/:${club}`}>
+      <div className="cursor-pointer" onClick={() => navigate(`/product/detail/${clubId}`)}>
         <img
           className="w-[14rem] h-[9rem] object-cover border-[0.05rem] border-cst-light-gray rounded-[8px]"
           src={clubThumbnailImg}
@@ -21,9 +21,9 @@ const SimpleClub = (props) => {
           {clubTitle}
         </strong>
         <span className="block mt-[0.4rem] mx-[0.2rem] text-[1.2rem] leading-[1.5rem] font-bold text-m-color">
-          {clubLocation}
+          {clubFee}
         </span>
-      </Link>
+      </div>
     </li>
   );
 };
