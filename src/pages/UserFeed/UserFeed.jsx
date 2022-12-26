112c3742187d3a7c4f08ce521a884aaee5e5cb24
jihyunglee <jhlee7125@gmail.com>
2022-12-26 21:18:56 +0900

    feat: url location에 따른 유저 피드 페이지 api 정보 받아오기

--- src/pages/UserFeed/UserFeed.jsx
+++ src/pages/UserFeed/UserFeed.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState, useCallback, useRef } from "react";
+import { useLocation } from "react-router-dom";
+import api from "../../api/axios";
 import { HeaderBasic } from "../../shared/Header/HeaderBasic";
 import UserProfile from "../../shared/Profile/UserProfile";
 import UserClub from "../../shared/UserClub/UserClub";
@@ -9,166 +11,21 @@ import Footer from "../../shared/Footer/Footer";
 import ModalInfo from "../../components/ModalModule/ModalInfo";
 import Modal from "../../components/ModalModule/Modal";
 
-const data = {
-  post: [
-    {
-      id: "1",
-      content:
-        "안녕하세요. 1 입니다. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum fugit harum et placeat libero tenetur, necessitatibus voluptas, labore deleniti accusantium maxime repellendus, sit eius exercitationem beatae corporis magni? Officia, aspernatur.",
-      image:
-        "https://images.unsplash.com/photo-1664574654521-7faf33eb9086?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-      createdAt: "2021-12-20T06:39:39.929Z",
-      updatedAt: "2021-12-20T08:39:39.929Z",
-      hearted: false,
-      heartCount: 1,
-      commentCount: 10,
-      author: {
-        _id: "1209309vw12123094tv",
-        username: "애월읍 위니브 감귤농장",
-        accountname: "weniv_Mandarin",
-        following: [],
-        follower: ["follower id"],
-        followerCount: 2350,
-        followingCount: 128,
-      },
-    },
-    {
-      id: "2",
-      content:
-        "안녕하세요. 2 입니다. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum fugit harum et placeat libero tenetur, necessitatibus voluptas, labore deleniti accusantium maxime repellendus, sit eius exercitationem beatae corporis magni? Officia, aspernatur.",
-      image:
-        "https://images.unsplash.com/photo-1671395781342-fd36baf0eda3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-      createdAt: "2021-12-31T06:39:39.929Z",
-      updatedAt: "2022-01-01T09:39:39.929Z",
-      hearted: true,
-      heartCount: 12,
-      commentCount: 43,
-      author: {
-        _id: "1209309vw12123094tv",
-        username: "애월읍 위니브 감귤농장",
-        accountname: "weniv_Mandarin",
-        following: [],
-        follower: ["follower id"],
-        followerCount: 2350,
-        followingCount: 128,
-      },
-    },
-    {
-      id: "3",
-      content:
-        "안녕하세요. 3 입니다. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum fugit harum et placeat libero tenetur, necessitatibus voluptas, labore deleniti accusantium maxime repellendus, sit eius exercitationem beatae corporis magni? Officia, aspernatur.",
-      image:
-        "https://images.unsplash.com/photo-1671044508138-91281343f2f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0N3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60, https://images.unsplash.com/photo-1671372675714-dbeff989d93c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60, https://images.unsplash.com/photo-1671212041182-2657b7b950bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3MHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-      createdAt: "2022-02-28T10:39:39.929Z",
-      updatedAt: "",
-      hearted: false,
-      heartCount: 32,
-      commentCount: 10,
-      author: {
-        _id: "1209309vw12123094tv",
-        username: "애월읍 위니브 감귤농장",
-        accountname: "weniv_Mandarin",
-        following: [],
-        follower: ["follower id"],
-        followerCount: 2350,
-        followingCount: 128,
-      },
-    },
-    {
-      id: "4",
-      content:
-        "안녕하세요. 4 입니다. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum fugit harum et placeat libero tenetur, necessitatibus voluptas, labore deleniti accusantium maxime repellendus, sit eius exercitationem beatae corporis magni? Officia, aspernatur.",
-      image:
-        "https://images.unsplash.com/photo-1671347202398-b09163f873bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4OHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60, https://plus.unsplash.com/premium_photo-1661398240310-c292420d7c02?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMDN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-      createdAt: "2022-03-28T10:39:39.929Z",
-      updatedAt: "2022-03-31T11:39:39.929Z",
-      hearted: false,
-      heartCount: 0,
-      commentCount: 0,
-      author: {
-        _id: "1209309vw12123094tv",
-        username: "애월읍 위니브 감귤농장",
-        accountname: "weniv_Mandarin",
-        following: [],
-        follower: ["follower id"],
-        followerCount: 2350,
-        followingCount: 128,
-      },
-    },
-    {
-      id: "5",
-      content:
-        "안녕하세요. 5 입니다. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum fugit harum et placeat libero tenetur, necessitatibus voluptas, labore deleniti accusantium maxime repellendus, sit eius exercitationem beatae corporis magni? Officia, aspernatur.",
-      image:
-        "https://images.unsplash.com/photo-1671017926057-6e8ca6048b36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-      createdAt: "2022-04-12T21:39:39.929Z",
-      updatedAt: "",
-      hearted: true,
-      heartCount: 0,
-      commentCount: 0,
-      author: {
-        _id: "1209309vw12123094tv",
-        username: "애월읍 위니브 감귤농장",
-        accountname: "weniv_Mandarin",
-        following: [],
-        follower: ["follower id"],
-        followerCount: 2350,
-        followingCount: 128,
-      },
-    },
-    {
-      id: "6",
-      content:
-        "안녕하세요. 6 입니다. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum fugit harum et placeat libero tenetur, necessitatibus voluptas, labore deleniti accusantium maxime repellendus, sit eius exercitationem beatae corporis magni? Officia, aspernatur.",
-      image:
-        "https://images.unsplash.com/photo-1671129823992-bd867dba6b12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-      createdAt: "2022-06-19T21:39:39.929Z",
-      updatedAt: "2022-09-11T21:39:39.929Z",
-      hearted: false,
-      heartCount: 0,
-      commentCount: 0,
-      author: {
-        _id: "1209309vw12123094tv",
-        username: "애월읍 위니브 감귤농장",
-        accountname: "weniv_Mandarin",
-        following: [],
-        follower: ["follower id"],
-        followerCount: 2350,
-        followingCount: 128,
-      },
-    },
-    {
-      id: "7",
-      content:
-        "안녕하세요. 7 입니다. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum fugit harum et placeat libero tenetur, necessitatibus voluptas, labore deleniti accusantium maxime repellendus, sit eius exercitationem beatae corporis magni? Officia, aspernatur.",
-      image:
-        "https://images.unsplash.com/photo-1670843837936-1e10a67239c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60, https://images.unsplash.com/photo-1670710029361-7446b0519c9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNzV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-      createdAt: "2022-08-11T14:39:39.929Z",
-      updatedAt: "",
-      hearted: true,
-      heartCount: 10,
-      commentCount: 23,
-      author: {
-        _id: "1209309vw12123094tv",
-        username: "애월읍 위니브 감귤농장",
-        accountname: "weniv_Mandarin",
-        following: [],
-        follower: ["follower id"],
-        followerCount: 2350,
-        followingCount: 128,
-      },
-    },
-  ],
-};
-
 const UserFeed = () => {
   const loadingImg = `${process.env.PUBLIC_URL}/assets/img/char-loading-cat.svg`;
+  const token = localStorage.getItem("token");
+  const [userProfile, setUserProfile] = useState();
   const [list, setList] = useState(true);
   const [postDataArray, setPostDataArray] = useState([]);
+  // const [follow, setFollow] = useState(false);
 
   const [modal, setModal] = useState(false);
   const [logout, setLogout] = useState(false);
   const modalRef = useRef();
+
+  const location = useLocation();
+  const accountname = location.pathname.split("/")[2];
+
   const handleModalInfo = useCallback(
     (e) => {
       setModal(!modal);
@@ -186,47 +43,112 @@ const UserFeed = () => {
   }, []);
 
   useEffect(() => {
-    if (postDataArray) {
-      const fetchData = async () => {
+    if (!userProfile) {
+      const getUserProfile = async () => {
         try {
-          await setPostDataArray(data.post);
-        } catch (err) {}
+          const res = await api.get(`/profile/${accountname}`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          setUserProfile(res.data.profile);
+          // setFollow(res.data.profile.isfollow);
+        } catch (err) {
+          console.log(err);
+        }
       };
-      fetchData();
+      getUserProfile();
     }
-  }, [postDataArray]);
+  }, [accountname, token, userProfile]);
+
+  useEffect(() => {
+    if (postDataArray.length === 0) {
+      const getUserFeeds = async () => {
+        try {
+          const res = await api.get(`/post/${accountname}/userpost`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          setPostDataArray(res.data.post);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
+      getUserFeeds();
+    }
+  }, [accountname, postDataArray, token]);
 
   const onListToggle = () => {
     setList(!list);
   };
 
+  // useEffect(() => {
+  //   if (!!userProfile) {
+  //     if (follow === userProfile.isfollow) {
+  //       return;
+  //     } else if (follow !== userProfile.isfollow && follow === true) {
+  //       const followReq = async () => {
+  //         const res = await api.post(`/profile/${accountname}/follow`, {
+  //           headers: {
+  //             Authorization: `Bearer ${token}`,
+  //           },
+  //         });
+  //         console.log(res.data.profile);
+  //         console.log("follow", follow);
+  //         console.log("userprofile.isfollow", userProfile.isfollow);
+  //       };
+  //       followReq();
+  //     } else if (follow !== userProfile.isfollow && follow === false) {
+  //       const unfollowReq = async () => {
+  //         const res = await api.delete(`/profile/${accountname}/unfollow`, {
+  //           headers: {
+  //             Authorization: `Bearer ${token}`,
+  //           },
+  //         });
+  //         console.log(res.data.profile);
+  //         console.log("follow", follow);
+  //         console.log("userprofile.isfollow", userProfile.isfollow);
+  //       };
+  //       unfollowReq();
+  //     }
+  //   }
+  // }, [follow, accountname, token, userProfile]);
+
   return (
     <div className="page">
       <HeaderBasic onModalInfo={handleModalInfo} />
       <main>
-        <UserProfile />
-        <UserClub />
-        <PostTypeSelectBar list={list} onListToggle={onListToggle} />
-        <section className="">
-          <h2 className="ir">유저 게시글</h2>
-          {postDataArray.length === 0 && (
-            <p className="mt-[30%] text-[2.4rem] text-center">아직 생성된 게시글이 없어요 ㅠㅠ</p>
-          )}
-          {postDataArray ? (
-            list ? (
-              postDataArray.map((post, idx) => <Post key={post.id} post={post} />)
-            ) : (
-              <section className="flex flex-wrap my-[1.6rem] mx-[1.6rem]">
-                <h2 className="ir">앨범형</h2>
-                {postDataArray.map((post, idx) => (
-                  <PostAlbum key={post.id} post={post} idx={idx} />
-                ))}
-              </section>
-            )
-          ) : (
-            <img src={loadingImg} alt="잠시만 기다려 주세요." />
-          )}
-        </section>
+        {userProfile ? (
+          <>
+            <UserProfile userProfile={userProfile} /* follow={follow} setFollow={setFollow} */ />
+            <UserClub />
+            <PostTypeSelectBar list={list} onListToggle={onListToggle} />
+            <section>
+              <h2 className="ir">유저 게시글</h2>
+              {postDataArray.length === 0 && (
+                <p className="mt-[30%] text-[2.4rem] text-center">아직 생성된 게시글이 없어요 ㅠㅠ</p>
+              )}
+              {postDataArray ? (
+                list ? (
+                  postDataArray.map((post, idx) => <Post key={post.id} post={post} />)
+                ) : (
+                  <section className="flex flex-wrap my-[1.6rem] mx-[1.6rem]">
+                    <h2 className="ir">앨범형</h2>
+                    {postDataArray.map((post, idx) => (
+                      <PostAlbum key={post.id} post={post} idx={idx} />
+                    ))}
+                  </section>
+                )
+              ) : (
+                <img src={loadingImg} alt="잠시만 기다려 주세요." />
+              )}
+            </section>
+          </>
+        ) : (
+          <></>
+        )}
       </main>
       <Footer />
       {modal === true ? (
