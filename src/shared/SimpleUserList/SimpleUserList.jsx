5a9bbde4dc71834941835899909254b2a788ba3f
jihyunglee <jhlee7125@gmail.com>
2022-12-26 10:35:28 +0900

    feat: 사용되는 모드에 따라 활성화되는 요소를 구분, handleLink URL 변경 및 user 정보에 대한 결과 props로 받아오기

--- src/shared/SimpleUserList/SimpleUserList.jsx
+++ src/shared/SimpleUserList/SimpleUserList.jsx
@@ -1,19 +1,17 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const SimpleUserList = ({ isBtn }) => {
+const SimpleUserList = ({ isMessage, isBtn, isChatMode, username, accountname, profileImg }) => {
   // Note::아래 변수는 임시로 구현했으며, 실제로는 API와 props를 사용합니다.
-  const tmpImg = `${process.env.PUBLIC_URL}/assets/img/profile-man-small.png`;
-  // const isBtn = true;
-  const userName = "사용자 이름";
+  // const profileImg = `${process.env.PUBLIC_URL}/assets/img/profile-man-small.png`;
+
   // Note::아래 셋 중 하나가 사용됩니다. nullish를 사용해서 0이 아닌 null 값이 들어가야 됩니다.
   // Note:: 실제로는 nullish가 아니라 props를 사용해 컨트롤할 수 있습니다.
-  const accountName = null;
   const content = null;
-  const chat = "안녕하세요. 가나다라마바사아자카타파하아";
+  const chat = isChatMode && "안녕하세요. 가나다라마바사아자카타파하아";
 
   // 채팅리스트에서만 사용됩니다. 읽지 않은 메시지 알람을 관립합니다.
-  const message = true;
+  // const message = true;
   const navigate = useNavigate();
 
   // Note::useState(false)안의 인자값을 props로 받고 이벤트 발생 시 axios로 데이터를 전송하는 건 어떨까요?
@@ -28,7 +26,7 @@ const SimpleUserList = ({ isBtn }) => {
     if (chat) {
       navigate("/chat/1");
     } else {
-      navigate("/profile/:accountname");
+      navigate(`/profile/${accountname}`);
     }
   };
 
@@ -36,19 +34,18 @@ const SimpleUserList = ({ isBtn }) => {
   return (
     <li
       className={`relative mx-[1.6rem] my-[1.6rem] flex justify-between items-center ${
-        message
+        isMessage
           ? "before:content-[''] before:bg-m-color before:w-[1.2rem] before:h-[1.2rem] before:absolute before:left-0 before:top-0 before:rounded-full"
           : ""
       } `}
     >
-      <img src={tmpImg} alt="" className="w-[5rem] h-[5rem] cursor-pointer" onClick={handleLink} />
+      <img src={profileImg} alt="" className="w-[5rem] h-[5rem] cursor-pointer" onClick={handleLink} />
       <p className="mr-auto ml-[1.2rem] cursor-pointer" onClick={handleLink}>
-        <strong className="font-medium">{userName}</strong>
+        <strong className="font-medium">{username}</strong>
         <span className="block text-[1.2rem] w-[23.8rem] whitespace-nowrap text-cst-gray text-ellipsis overflow-hidden">
-          {accountName ?? content ?? chat}
+          {`@ ${accountname}` ?? content ?? chat}
         </span>
       </p>
-
       {isBtn ? (
         <button
           onClick={handleIsFollow}
@@ -58,11 +55,14 @@ const SimpleUserList = ({ isBtn }) => {
           {isFollow ? "팔로우" : "취소"}
         </button>
       ) : (
-        <>
-          <time dateTime="2022-12-21" className="mt-[2.3rem] text-[1rem] text-cst-light-gray">
-            2022.12.21
-          </time>
-        </>
+        <></>
+      )}
+      {isChatMode ? (
+        <time dateTime="2022-12-21" className="mt-[2.3rem] text-[1rem] text-cst-light-gray">
+          2022.12.21
+        </time>
+      ) : (
+        <></>
       )}
     </li>
   );
