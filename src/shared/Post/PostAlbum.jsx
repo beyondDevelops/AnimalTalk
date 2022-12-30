c49dfe1530cd50e2555fad1d139b2f182f985450
GreattitJY <great.tit.jaejae@gmail.com>
2022-12-30 21:11:04 +0900

    fix: 초기 렌더링 undefined.includes() 오류 해결 및 모달 수정

--- src/shared/Post/PostAlbum.jsx
+++ src/shared/Post/PostAlbum.jsx
@@ -2,59 +2,53 @@ import React, { useState } from "react";
 import ModalPostImg from "../../components/ModalModule/ModalPostImg";
 
 const PostAlbum = ({ post, idx }) => {
-  const [modal, setModal] = useState(false);
+  const [modalPostImg, setModalPostImg] = useState(false);
 
   const imgLayersImg = `${process.env.PUBLIC_URL}/assets/img/icon-img-layers.png`;
-
-  const handleModalToggle = () => {
-    modal ? setModal(false) : setModal(true);
-  };
 
   const postImg = !!post.image.split(",")[0] ? post.image : null;
 
   return (
-    <div
-      className={`relative overflow-hidden w-[11.4rem] h-[11.4rem] mb-[0.8rem] ${
-        (idx + 1) % 3 === 0 ? "mr-0" : "mr-[0.8rem]"
-      } rounded-[10px]`}
-    >
-      <div className="relative flex h-full">
-        {postImg && postImg.includes(",") ? (
-          <>
-            <button type="button" className="absolute top-[0.6rem] right-[0.6rem]">
-              <img src={imgLayersImg} alt="" className="w-[2rem] h-[2rem]" />
-            </button>
-            {postImg.split(",").map((img) => (
+    <>
+      <div
+        className={`relative overflow-hidden w-[11.4rem] h-[11.4rem] mb-[0.8rem] ${
+          (idx + 1) % 3 === 0 ? "mr-0" : "mr-[0.8rem]"
+        } rounded-[10px]`}
+      >
+        <div className="relative flex h-full">
+          {postImg && postImg.includes(",") ? (
+            <>
+              <button type="button" className="absolute top-[0.6rem] right-[0.6rem]">
+                <img src={imgLayersImg} alt="" className="w-[2rem] h-[2rem]" />
+              </button>
+              {postImg.split(",").map((img) => (
+                <img
+                  key={crypto.randomUUID()}
+                  src={!!img && img.includes("https") ? img : `https://mandarin.api.weniv.co.kr/${img}`}
+                  alt=""
+                  className="min-w-full object-cover cursor-pointer"
+                  onClick={() => {
+                    setModalPostImg(!modalPostImg);
+                  }}
+                />
+              ))}
+            </>
+          ) : (
+            <>
               <img
-                src={img.includes("https") ? img : `https://mandarin.api.weniv.co.kr/${img}`}
+                src={!!postImg && postImg.includes("https") ? postImg : `https://mandarin.api.weniv.co.kr/${postImg}`}
                 alt=""
                 className="min-w-full object-cover cursor-pointer"
-                onClick={handleModalToggle}
+                onClick={() => {
+                  setModalPostImg(!modalPostImg);
+                }}
               />
-            ))}
-            {modal ? (
-              <ModalPostImg imgArr={postImg.split(",")} modal={modal} onModalToggle={handleModalToggle} />
-            ) : (
-              <></>
-            )}
-          </>
-        ) : (
-          <>
-            <img
-              src={postImg.includes("https") ? postImg : `https://mandarin.api.weniv.co.kr/${postImg}`}
-              alt=""
-              className="min-w-full object-cover cursor-pointer"
-              onClick={handleModalToggle}
-            />
-            {modal ? (
-              <ModalPostImg imgArr={postImg.split(",")} modal={modal} onModalToggle={handleModalToggle} />
-            ) : (
-              <></>
-            )}
-          </>
-        )}
+            </>
+          )}
+        </div>
       </div>
-    </div>
+      {modalPostImg ? <ModalPostImg imgArr={postImg.split(",")} {...{ setModalPostImg }} {...{ post }} /> : <></>}
+    </>
   );
 };
 
