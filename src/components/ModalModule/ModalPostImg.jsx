1d92aadb8022f89ccae0c954d8614acbbde76d34
GreattitJY <great.tit.jaejae@gmail.com>
2022-12-30 17:50:44 +0900

    refactor: 게시글 모달창 화면 디자인 및 이벤트 변경

--- src/components/ModalModule/ModalPostImg.jsx
+++ src/components/ModalModule/ModalPostImg.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 
-const ModalPostImg = ({ imgArr, modal, onModalToggle }) => {
+const ModalPostImg = ({ imgArr, modalPostImage, setModalPostImage, post }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const leftArrow = useRef(null);
   const rightArrow = useRef(null);
@@ -21,58 +21,67 @@ const ModalPostImg = ({ imgArr, modal, onModalToggle }) => {
     }
   };
 
+  const modalRef = useRef();
+  const handleModal = (e) => {
+    if (e.target === modalRef.current) {
+      setModalPostImage(false);
+    }
+  };
+
   return (
-    <div
-      className={`fixed top-0 left-0 w-screen h-screen grid place-items-center transition-all ease-linear duration-1000 bg-gray-800 bg-opacity-50 ${
-        modal ? "visible z-10" : "invisible -z-10"
-      }`}
-    >
-      <button
-        className={`absolute top-[50%] left-[6%] min-[391px]:left-[10%] lg:left-[25%] z-30 ${
-          imgArr.length > 1 ? "bg-[#ffffffdb] hover:text-black" : "bg-[#ffffff00]"
-        } leading-[100%] w-[3rem] h-[3rem] text-[3rem] text-cst-gray rounded-[50%] cursor-pointer`}
-        type="button"
-        aria-label="prev"
-        data-name="prev"
-        onClick={(e) => handleModalCarousel(e.target)}
-        ref={leftArrow}
-      >
-        &lt;
-      </button>
-      <section className="w-[70%] items-center grid place-items-center overflow-hidden relative">
-        <h3 className="ir">이미지</h3>
-        <div className="relative flex flex-row h-full overflow-x-scroll scrollbar-hide">
-          <img
-            src={
-              imgArr[currentIndex].includes("https")
-                ? imgArr[currentIndex]
-                : `https://mandarin.api.weniv.co.kr/${imgArr[currentIndex]}`
-            }
-            alt=""
-            className="min-w-full object-contain"
-          />
-          <button
-            type="button"
-            className="absolute top-[1rem] right-[1rem] text-[2.4rem] font-bold bg-transparent border-transparent text-[#fff] cursor-pointer transition-all ease-linear duration-300 hover:text-m-color hover:scale-125"
-            onClick={onModalToggle}
-          >
-            X
-          </button>
+    <section className="absolute inset-0 z-10" ref={modalRef} onClick={handleModal}>
+      <h3 className="ir">게시글 사진 모달</h3>
+
+      <div className="absolute bottom-0 left-[50%] -translate-x-[50%] w-[39rem] h-[60%] rounded-t-[10px] bg-slate-100/60">
+        <button
+          type="button"
+          aria-label="닫기창"
+          className="relative w-[100%] h-[3.6rem] border-b-[0.5rem] border-b-s-color/80 before:absolute before:left-[50%] before:-translate-x-[50%] before:content-[''] before:w-[5rem] before:h-[0.4rem] before:bg-cst-light-gray before:hover:bg-s-color"
+          onClick={() => {
+            setModalPostImage(false);
+          }}
+        ></button>
+
+        {/* 이미지 캐러셀 */}
+        <div className="relative mx-auto my-[2rem] overflow-hidden w-[30.4rem] h-[22.8rem] rounded-[10px]">
+          <div className=" flex flex-row h-full overflow-x-scroll scrollbar-hide">
+            <img
+              src={
+                imgArr[currentIndex].includes("https")
+                  ? imgArr[currentIndex]
+                  : `https://mandarin.api.weniv.co.kr/${imgArr[currentIndex]}`
+              }
+              alt=""
+              className="min-w-full object-cover"
+            />
+            <button
+              className={`absolute top-[50%] left-[6%] min-[391px]:left-[10%] lg:left-[25%] z-30 ${
+                imgArr.length > 1 ? "bg-[#ffffffdb] hover:text-black" : "bg-[#ffffff00]"
+              } leading-[100%] w-[3rem] h-[3rem] text-[3rem] text-cst-gray rounded-[50%] cursor-pointer`}
+              type="button"
+              aria-label="prev"
+              data-name="prev"
+              onClick={(e) => handleModalCarousel(e.target)}
+              ref={leftArrow}
+            >
+              &lt;
+            </button>
+            <button
+              className={`absolute top-[50%] right-[6%] min-[391px]:right-[10%] lg:right-[25%] z-30 ${
+                imgArr.length > 1 ? "bg-[#ffffffdb] hover:text-black" : "bg-[#ffffff00]"
+              } leading-[100%] w-[3rem] h-[3rem] text-[3rem] text-cst-gray rounded-[50%] cursor-pointer`}
+              type="button"
+              aria-label="next"
+              data-name="next"
+              onClick={(e) => handleModalCarousel(e.target)}
+              ref={rightArrow}
+            >
+              &gt;
+            </button>
+          </div>
         </div>
-      </section>
-      <button
-        className={`absolute top-[50%] right-[6%] min-[391px]:right-[10%] lg:right-[25%] z-30 ${
-          imgArr.length > 1 ? "bg-[#ffffffdb] hover:text-black" : "bg-[#ffffff00]"
-        } leading-[100%] w-[3rem] h-[3rem] text-[3rem] text-cst-gray rounded-[50%] cursor-pointer`}
-        type="button"
-        aria-label="next"
-        data-name="next"
-        onClick={(e) => handleModalCarousel(e.target)}
-        ref={rightArrow}
-      >
-        &gt;
-      </button>
-    </div>
+      </div>
+    </section>
   );
 };
 
--- src/shared/Post/Post.jsx
+++ src/shared/Post/Post.jsx
@@ -1,11 +1,12 @@
-import React, { useRef, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import ModalPostImg from "../../components/ModalModule/ModalPostImg";
+import { UserContext } from "../../context/UserContext";
 
 const Post = ({ post }) => {
   // 아래 이미지 변수는 기본 설정입니다.
   const profileSmallImg =
-    post.author.image && post.author.image !== "http://146.56.183.55:5050/Ellipse.png"
+    post.author.image !== "http://146.56.183.55:5050/Ellipse.png"
       ? post.author.image
       : `${process.env.PUBLIC_URL}/assets/img/profile-man-small.png`;
   const moreVerticalSmallImg = `${process.env.PUBLIC_URL}/assets/img/icon-more-vertical-small.png`;
@@ -13,14 +14,20 @@ const Post = ({ post }) => {
   const heartOnImg = `${process.env.PUBLIC_URL}/assets/img/icon-heart-on.png`;
   const commentImg = `${process.env.PUBLIC_URL}/assets/img/icon-message-circle-line-profile.png`;
 
-  // 이미지 관리 및 모달 처리
+  // 포스트 이미지 관리 및 모달 처리
   const postImg = !!post.image.split(",")[0] ? post.image : null;
   const imgRef = useRef(null);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [modal, setModal] = useState(false);
+  const [modalPostImage, setModalPostImage] = useState(false);
 
-  const handleModalToggle = () => {
-    modal ? setModal(false) : setModal(true);
+  // 날짜 정보 관리
+  const postDate = post.updatedAt.slice(0, 10).replaceAll("-", "");
+  const year = postDate.slice(0, 4);
+  const month = postDate.slice(4, 6);
+  const date = postDate.slice(6, 8);
+
+  const handleModalPostImage = () => {
+    modalPostImage ? setModalPostImage(false) : setModalPostImage(true);
   };
 
   // 좋아요 관리x
@@ -34,112 +41,115 @@ const Post = ({ post }) => {
 
   const handleLink = () => navigate(`/profile/${post.author.accountname}`);
 
-  // 날짜 정보 관리
-  const postDate = post.updatedAt.slice(0, 10).replaceAll("-", "");
-  const year = postDate.slice(0, 4);
-  const month = postDate.slice(4, 6);
-  const date = postDate.slice(6, 8);
+  const { _id } = useContext(UserContext);
+
+  const handleModal = () => {
+    console.log("로그인유저", _id);
+    console.log("게시자", post.author._id);
+  };
 
   return (
-    <section className="my-[2rem] mx-[1.6rem]">
-      <img
-        className="inline-block w-[4.2rem] h-[4.2rem] cursor-pointer"
-        src={profileSmallImg}
-        alt={`${post.author.username}님 방문하기`}
-        onClick={handleLink}
-      />
-      <p className="inline-block align-middle ml-[1.2rem] cursor-pointer" onClick={handleLink}>
-        <strong className="leading-[1.8rem] font-medium whitespace-nowrap overflow-hidden overflow-ellipsis">
-          {post.author.username}
-        </strong>
-        <span className="block mt-[0.2rem] text-[1.2rem] leading-[1rem] text-cst-gray">
-          @&nbsp;{post.author.accountname}
-        </span>
-      </p>
-      <img
-        className="float-right w-[1.8rem] h-[1.8rem] mt-[0.4rem] cursor-pointer"
-        src={moreVerticalSmallImg}
-        alt="더보기"
-      />
+    <>
+      <section className="my-[2rem] mx-[1.6rem]">
+        <img
+          tabIndex="0"
+          className="inline-block w-[4.2rem] h-[4.2rem] rounded-full object-cover cursor-pointer"
+          src={profileSmallImg}
+          alt={`${post.author.username}님 프로필 사진입니다. 클릭 시 해당 프로필 유저 페이지로 이동합니다.`}
+          onClick={handleLink}
+        />
+        <p className="inline-block align-middle ml-[1.2rem] cursor-pointer" onClick={handleLink}>
+          <strong className="leading-[1.8rem] font-medium whitespace-nowrap overflow-hidden overflow-ellipsis">
+            {post.author.username}
+          </strong>
+          <span className="block mt-[0.2rem] text-[1.2rem] leading-[1rem] text-cst-gray">
+            @ {post.author.accountname}
+          </span>
+        </p>
 
-      <p className={`leading-[1.8rem] mt-[1.6rem] ml-[5.4rem] ${postImg ? "" : "mb-[1.4rem]"}`}>{post.content}</p>
+        <button type="button" className="float-right mt-[0.6rem]" onClick={handleModal}>
+          <img className="w-[1.8rem] h-[1.8rem]" src={moreVerticalSmallImg} alt="더보기" />
+        </button>
 
-      {postImg ? (
-        <div className="relative overflow-hidden w-[30.4rem] h-[22.8rem] mt-[1.6rem] ml-[5.4rem] mb-[1.4rem] rounded-[10px]">
-          {postImg && postImg.includes(",") ? (
-            <>
-              <div className="flex flex-row h-full overflow-x-scroll scrollbar-hide" ref={imgRef}>
-                {postImg.split(",").map((img, idx) => (
-                  <img
-                    key={idx}
-                    src={img.includes("https") ? img : `https://mandarin.api.weniv.co.kr/${img}`}
-                    alt=""
-                    className={`min-w-full object-cover cursor-pointer ${
-                      postImg.split(",")[currentIndex] === img ? "" : "hidden"
-                    }`}
-                    onClick={handleModalToggle}
-                  />
-                ))}
-                {modal ? (
-                  <ModalPostImg imgArr={postImg.split(",")} modal={modal} onModalToggle={handleModalToggle} />
-                ) : (
-                  <></>
-                )}
-              </div>
-              <div className="relative flex justify-center -translate-y-[2rem]">
-                {postImg.split(",").map((img, idx) => (
-                  <button
-                    onClick={() => setCurrentIndex(postImg.split(",").indexOf(img))}
-                    key={idx}
-                    id={idx}
-                    className={`mr-[0.6rem] last:mr-0 text-white w-[0.6rem] h-[0.6rem] rounded-[50%] ${
-                      postImg.split(",")[currentIndex] === img ? "bg-m-color" : "bg-white"
-                    }`}
-                  ></button>
-                ))}
-              </div>
-            </>
-          ) : (
-            <>
+        <p className={`leading-[1.8rem] mt-[1.6rem] ml-[5.4rem] ${postImg ? "" : "mb-[1.4rem]"}`}>{post.content}</p>
+
+        {/* 이미지 캐러셀 부분 */}
+        {postImg ? (
+          <div className="relative overflow-hidden w-[30.4rem] h-[22.8rem] mt-[1.6rem] ml-[5.4rem] mb-[1.4rem] rounded-[10px]">
+            {postImg.includes(",") ? (
+              <>
+                <div className="flex flex-row h-full overflow-x-scroll scrollbar-hide" ref={imgRef}>
+                  {postImg.split(",").map((img, idx) => (
+                    <img
+                      key={idx}
+                      src={img.includes("https") ? img : `https://mandarin.api.weniv.co.kr/${img}`}
+                      alt=""
+                      className={`min-w-full object-cover cursor-pointer ${
+                        postImg.split(",")[currentIndex] === img ? "" : "hidden"
+                      }`}
+                      onClick={handleModalPostImage}
+                    />
+                  ))}
+                </div>
+                <div className="relative flex justify-center -translate-y-[2rem]">
+                  {postImg.split(",").map((img, idx) => (
+                    <button
+                      onClick={() => setCurrentIndex(postImg.split(",").indexOf(img))}
+                      key={idx}
+                      id={idx}
+                      className={`mr-[0.6rem] last:mr-0 text-white w-[0.6rem] h-[0.6rem] rounded-[50%] ${
+                        postImg.split(",")[currentIndex] === img ? "bg-m-color" : "bg-white"
+                      }`}
+                    ></button>
+                  ))}
+                </div>
+              </>
+            ) : (
               <div className="flex h-full">
                 <img
                   src={postImg.includes("https") ? postImg : `https://mandarin.api.weniv.co.kr/${postImg}`}
                   alt=""
                   className="min-w-full object-cover cursor-pointer"
-                  onClick={handleModalToggle}
+                  onClick={handleModalPostImage}
                 />
               </div>
-              {modal ? (
-                <ModalPostImg imgArr={postImg.split(",")} modal={modal} onModalToggle={handleModalToggle} />
-              ) : (
-                <></>
-              )}
-            </>
-          )}
-        </div>
+            )}
+          </div>
+        ) : (
+          <></>
+        )}
+
+        <button
+          type="button"
+          onClick={handleLikeBtn}
+          className="inline-flex items-center ml-[5.4rem] mr-[1.8rem] text-cst-gray align-bottom"
+        >
+          <img className="w-[2rem] h-[2rem]" src={`${isLike ? heartOnImg : heartOffImg}`} alt="좋아요" />
+          <span className="text-[1.2rem] leading-[1.2rem] ml-[0.6rem]">{post.heartCount}</span>
+        </button>
+        <Link to="/chat" className="inline-flex items-center text-cst-gray align-bottom">
+          <img className="w-[2rem] h-[2rem]" src={commentImg} alt="댓글 확인하기" />
+          <span className="text-[1.2rem leading-[1.2rem] ml-[0.6rem]">{post.commentCount}</span>
+        </Link>
+        <time
+          dateTime={`${year}-${month}-${date}`}
+          className="block ml-[5.4rem] mt-[1.8rem] mb-[0.4rem] text-[1rem] leading-[1.2rem] text-[#767676]"
+        >
+          {year}년 {month}월 {date}일
+        </time>
+      </section>
+
+      {modalPostImage ? (
+        <ModalPostImg
+          imgArr={postImg.split(",")}
+          setModalPostImage={setModalPostImage}
+          onModalToggle={handleModalPostImage}
+          {...{ post }}
+        />
       ) : (
         <></>
       )}
-
-      <button
-        type="button"
-        onClick={handleLikeBtn}
-        className="inline-flex items-center ml-[5.4rem] mr-[1.8rem] text-cst-gray align-bottom"
-      >
-        <img className="w-[2rem] h-[2rem]" src={`${isLike ? heartOnImg : heartOffImg}`} alt="좋아요" />
-        <span className="text-[1.2rem] leading-[1.2rem] ml-[0.6rem]">{post.heartCount}</span>
-      </button>
-      <Link to="/chat" className="inline-flex items-center text-cst-gray align-bottom">
-        <img className="w-[2rem] h-[2rem]" src={commentImg} alt="댓글 확인하기" />
-        <span className="text-[1.2rem leading-[1.2rem] ml-[0.6rem]">{post.commentCount}</span>
-      </Link>
-      <time
-        dateTime={`${year}-${month}-${date}`}
-        className="block ml-[5.4rem] mt-[1.8rem] mb-[0.4rem] text-[1rem] leading-[1.2rem] text-[#767676]"
-      >
-        {year}년 {month}월 {date}일
-      </time>
-    </section>
+    </>
   );
 };
 
