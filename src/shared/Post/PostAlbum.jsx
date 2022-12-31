1c0a3502b52956487205df5ea8620894b8b5ee23
GreattitJY <great.tit.jaejae@gmail.com>
2022-12-31 13:17:30 +0900

    fix: 이미지 null로 들어올 경우 렌더링 오류 수정 (fragment 반환)

--- src/shared/Post/PostAlbum.jsx
+++ src/shared/Post/PostAlbum.jsx
@@ -1,53 +1,54 @@
 import React, { useState } from "react";
 import ModalPostImg from "../../components/ModalModule/ModalPostImg";
 
-const PostAlbum = ({ post, idx }) => {
-  const [modalPostImg, setModalPostImg] = useState(false);
-
+const PostAlbum = ({ post }) => {
   const imgLayersImg = `${process.env.PUBLIC_URL}/assets/img/icon-img-layers.png`;
-
   const postImg = !!post.image.split(",")[0] ? post.image : null;
+  const [modalPostImg, setModalPostImg] = useState(false);
 
   return (
     <>
-      <div
-        className={`relative overflow-hidden w-[11.4rem] h-[11.4rem] mb-[0.8rem] ${
-          (idx + 1) % 3 === 0 ? "mr-0" : "mr-[0.8rem]"
-        } rounded-[10px]`}
-      >
-        <div className="relative flex h-full">
-          {postImg && postImg.includes(",") ? (
-            <>
-              <button type="button" className="absolute top-[0.6rem] right-[0.6rem]">
-                <img src={imgLayersImg} alt="" className="w-[2rem] h-[2rem]" />
-              </button>
-              {postImg.split(",").map((img) => (
-                <img
-                  key={crypto.randomUUID()}
-                  src={!!img && img.includes("https") ? img : `https://mandarin.api.weniv.co.kr/${img}`}
-                  alt=""
-                  className="min-w-full object-cover cursor-pointer"
-                  onClick={() => {
-                    setModalPostImg(!modalPostImg);
-                  }}
-                />
-              ))}
-            </>
-          ) : (
-            <>
-              <img
-                src={!!postImg && postImg.includes("https") ? postImg : `https://mandarin.api.weniv.co.kr/${postImg}`}
-                alt=""
-                className="min-w-full object-cover cursor-pointer"
-                onClick={() => {
-                  setModalPostImg(!modalPostImg);
-                }}
-              />
-            </>
-          )}
-        </div>
-      </div>
-      {modalPostImg ? <ModalPostImg imgArr={postImg.split(",")} {...{ setModalPostImg }} {...{ post }} /> : <></>}
+      {postImg ? (
+        <>
+          {/* 이미지 캐러셀  */}
+          <div className={`relative overflow-hidden w-[11.4rem] h-[11.4rem] rounded-[10px]`}>
+            <div className="relative flex h-full">
+              {postImg.includes(",") ? (
+                <>
+                  <img src={imgLayersImg} alt="" className="absolute top-[0.6rem] right-[0.6rem] w-[2rem] h-[2rem]" />
+                  {postImg.split(",").map((img) => (
+                    <img
+                      key={crypto.randomUUID()}
+                      src={img.includes("https") ? img : `https://mandarin.api.weniv.co.kr/${img}`}
+                      alt=""
+                      className="min-w-full object-cover cursor-pointer"
+                      onClick={() => {
+                        setModalPostImg(!modalPostImg);
+                      }}
+                    />
+                  ))}
+                </>
+              ) : (
+                <>
+                  <img
+                    src={postImg.includes("https") ? postImg : `https://mandarin.api.weniv.co.kr/${postImg}`}
+                    alt=""
+                    className="min-w-full object-cover cursor-pointer"
+                    onClick={() => {
+                      setModalPostImg(!modalPostImg);
+                    }}
+                  />
+                </>
+              )}
+            </div>
+          </div>
+
+          {/* 모달창 */}
+          {modalPostImg ? <ModalPostImg imgArr={postImg.split(",")} {...{ setModalPostImg }} {...{ post }} /> : <></>}
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
