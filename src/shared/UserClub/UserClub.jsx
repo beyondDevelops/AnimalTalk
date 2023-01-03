560d68467c50d84dc3f4b644137f54fde263705d
GreattitJY <great.tit.jaejae@gmail.com>
2023-01-03 22:44:25 +0900

    Docs: 리드미 파일 작업중

--- src/shared/UserClub/UserClub.jsx
+++ src/shared/UserClub/UserClub.jsx
@@ -45,55 +45,46 @@ const UserClub = ({ club, setIsUpload }) => {
   const [clubData, setClubData] = useState();
 
   return (
-    <>
-      <section className="relative px-[1.6rem] py-[2rem]">
-        <h2 className="mb-[1.6rem]">참여 중인 산책</h2>
-        <ul className="flex flex-row overflow-hidden overflow-x-scroll scrollbar-hide" ref={ulRef}>
-          {club.length > 0 ? (
-            club.map((item) => (
-              <SimpleClub key={item.id} data={item} {...{ setIsClubModal }} {...{ setClubData }} {...{ comma }} />
-            ))
-          ) : (
-            <li className="shrink-0 mr-[1rem] overflow-hidden text-m-color">현재 참여 중인 산책 모임이 없습니다.</li>
-          )}
-        </ul>
-        {club.length > 2 && (
-          <div className="relative top-[-9rem] flex justify-between w-full">
-            <button
-              className={`absolute z-10 left-[-1rem] ${
-                leftBtnVisible ? "visible" : "hidden"
-              } bg-[#0000005d] leading-[100%]  w-[3rem] h-[3rem] text-[2rem] text-m-color rounded-full hover:bg-m-color hover:text-s-color`}
-              type="button"
-              aria-label="prev"
-              onClick={() => {
-                handleHorizontalScroll(ulRef.current, 25, 150, -10);
-                handleArrowBtnVisibility(ulRef.current.scrollLeft - 150);
-              }}
-            >
-              &lt;
-            </button>
-            <button
-              className={`absolute z-10 right-[-1rem] ${
-                rightBtnVisible ? "visible" : "hidden"
-              } bg-[#0000005d] leading-[100%]  w-[3rem] h-[3rem] text-[2rem] text-m-color rounded-full hover:bg-m-color hover:text-s-color`}
-              type="button"
-              aria-label="next"
-              onClick={() => {
-                handleHorizontalScroll(ulRef.current, 25, 150, 10);
-                handleArrowBtnVisibility(ulRef.current.scrollLeft + 150);
-              }}
-            >
-              &gt;
-            </button>
-          </div>
+    <section className="relative px-[1.6rem] py-[2rem]">
+      <h2 className="mb-[1.6rem]">참여 중인 모임</h2>
+      <ul className="flex flex-row overflow-hidden overflow-x-scroll scrollbar-hide" ref={ulRef}>
+        {club.length > 0 ? (
+          club.map((item) => <SimpleClub key={item.id} data={item} />)
+        ) : (
+          <li className="shrink-0 mr-[1rem] overflow-hidden text-m-color">현재 참여 중인 산책 모임이 없습니다.</li>
         )}
-      </section>
-      {isClubModal ? (
-        <UserClubModal {...{ setIsClubModal }} {...{ clubData }} {...{ comma }} {...{ setIsUpload }} />
-      ) : (
-        <></>
+      </ul>
+      {club.length > 2 && (
+        <div className="relative top-[-9rem] flex justify-between w-full">
+          <button
+            className={`absolute z-10 left-[-1rem] ${
+              leftBtnVisible ? "visible" : "hidden"
+            } bg-[#0000005d] leading-[100%]  w-[3rem] h-[3rem] text-[2rem] text-m-color rounded-full hover:bg-m-color hover:text-s-color`}
+            type="button"
+            aria-label="prev"
+            onClick={() => {
+              handleHorizontalScroll(ulRef.current, 25, 150, -10);
+              handleArrowBtnVisibility(ulRef.current.scrollLeft - 150);
+            }}
+          >
+            &lt;
+          </button>
+          <button
+            className={`absolute z-10 right-[-1rem] ${
+              rightBtnVisible ? "visible" : "hidden"
+            } bg-[#0000005d] leading-[100%]  w-[3rem] h-[3rem] text-[2rem] text-m-color rounded-full hover:bg-m-color hover:text-s-color`}
+            type="button"
+            aria-label="next"
+            onClick={() => {
+              handleHorizontalScroll(ulRef.current, 25, 150, 10);
+              handleArrowBtnVisibility(ulRef.current.scrollLeft + 150);
+            }}
+          >
+            &gt;
+          </button>
+        </div>
       )}
-    </>
+    </section>
   );
 };
 
