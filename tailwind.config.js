4cd45d46889d6f3ee4dcaee0f3a5b5bdfc2e9cc9
jihyunglee <jhlee7125@gmail.com>
2023-03-28 17:16:12 +0900

    setting: ts.config.js에서 screens 설정을 통한 반응형 커스텀

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     screens: {
-      xs: '280px',
-      sm: '390px',
-      md: '440px',
-      lg: '1024px',
-      xlg: '1280px',
+      xs: "280px",
+      sm: "390px",
+      md: "440px",
+      lg: "1024px",
+      xlg: "1280px",
     },
     extend: {
       colors: {
-        'm-color': '#EDA751',
-        's-color': '#FCD690',
-        'cst-gray': '#767676',
-        'cst-light-gray': '#dbdbdb',
-        'cst-layout': '#f3f4f6',
+        "m-color": "#EDA751",
+        "s-color": "#FCD690",
+        "cst-gray": "#767676",
+        "cst-light-gray": "#dbdbdb",
+        "cst-layout": "#f3f4f6",
       },
       fontFamily: {
-        spoqa: ['Spoqa Han Sans Neo', 'sans-serif'],
+        spoqa: ["Spoqa Han Sans Neo", "sans-serif"],
       },
       keyframes: {
-        'fade-in': {
-          '0%': {
-            opacity: '0',
+        "fade-in": {
+          "0%": {
+            opacity: "0",
           },
-          '100%': {
-            opacity: '1',
+          "100%": {
+            opacity: "1",
           },
         },
       },
       animation: {
-        'fade-in': 'fade-in 2s ease-out',
+        "fade-in": "fade-in 2s ease-out",
       },
     },
   },
-  plugins: [require('tailwind-scrollbar-hide')],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
