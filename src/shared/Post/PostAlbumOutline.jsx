4ef952cc5fb4adad83b9f14fea89a17f79a562d9
jihyunglee <jhlee7125@gmail.com>
2023-04-07 20:34:17 +0900

    design: PostAlbum의 레이아웃 디자인을 별도의 Outline 컴포넌트에서 도입하여 Post 컴포넌트의 코드 가독성 향상을 기함

---
+++ src/shared/Post/PostAlbumOutline.jsx
@@ -0,0 +1,13 @@
+import { v4 as uuidv4 } from "uuid";
+import PostAlbum from "./PostAlbum";
+
+export const PostAlbumOutline = ({ results }) => {
+  return (
+    <section className="sm:grid sm:grid-cols-3 sm:gap-[0.8rem] my-[1.6rem] mx-[2rem]">
+      <h2 className="ir">앨범형</h2>
+      {results.map((post) => (
+        <PostAlbum key={post.id + uuidv4()} post={post} />
+      ))}
+    </section>
+  );
+};
