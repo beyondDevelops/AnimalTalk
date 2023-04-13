f077f7acd6421851289d056f453e610ef6f26811
jihyunglee <jhlee7125@gmail.com>
2023-04-14 02:42:56 +0900

    fix: readFollowingFeed, readMyFeed api 통신시 skip 갯수 10개로 조정

--- src/api/Feed/readFollowingFeed.ts
+++ src/api/Feed/readFollowingFeed.ts
@@ -1,6 +1,8 @@
 import { accessInstance } from "../axios";
 
 export const readFollowingFeed = async (feedParam: number) => {
-  const data = await accessInstance.get<any, any>(`/post/feed?limit=20&skip=${feedParam * 20}`);
+  console.log(feedParam);
+
+  const data = await accessInstance.get<any, any>(`/post/feed?limit=20&skip=${feedParam * 10}`);
   return data.posts;
 };
